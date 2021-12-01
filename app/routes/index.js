8d6659fd0b3a91aea703d8b419911ebb8a580a9c
Matt <matthew.k.guthrie@gmail.com>
2021-12-01 15:22:45 -0700

    updated artist component and style

--- app/routes/index.js
+++ app/routes/index.js
@@ -6,13 +6,20 @@ let token = encodeURIComponent(ENV.SPOTIFY_ACCESS_TOKEN);
 
 export default class IndexRoute extends Route {
   async model() {
-    let response = await fetch(
-      'https://api.spotify.com/v1/me/top/artists?time_range=medium_term&limit=10&offset=5',
-      {
-        method: 'GET',
-        headers: { Authorization: 'Bearer ' + token },
-      }
-    );
+    // let response = await fetch(
+    //   'https://api.spotify.com/v1/me/top/artists?time_range=medium_term&limit=10&offset=5',
+    //   {
+    //     method: 'GET',
+    //     headers: { Authorization: 'Bearer ' + token },
+    //   }
+    // );
+
+    // let { items } = await response.json();
+    //   console.log(items)
+    // return items;
+    
+    // }
+    let response = await fetch('/api/spotify.json');
 
     let { items } = await response.json();
       console.log(items)
@@ -20,3 +27,6 @@ export default class IndexRoute extends Route {
     
     }
 }
+
+
+
